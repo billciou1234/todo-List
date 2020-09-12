8644727114350bf3ae278b41a883292e8082f66e
Bill <reborn800201@gmail.com>
2020-09-12 20:05:34 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
 const exphbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
+const app = express()
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -17,7 +17,7 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // 設定首頁路由
@@ -26,8 +26,19 @@ app.get('/', (req, res) => {
     .lean()
     .then(todos => res.render('index', { todos: todos }))
     .catch(error => console.error(error))
+})
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
 
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+
+  const todo = new Todo({ name })
+  return todo.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
