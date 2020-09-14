d62e5d6d84b386a2bcc462ae4d6d87a4597cefa7
Bill <reborn800201@gmail.com>
2020-09-14 20:01:10 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')
-const Todo = require('./models/todo')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-// 載入 method-override
 const methodOverride = require('method-override')
-
 const routes = require('./routes')
-const router = require('./routes')
 const app = express()
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+require('./config/mongooes')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongdb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
-
-
-
 // 設定首頁路由
 app.ues(routes)
-
-
-
-
-
-
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,16 +1,8 @@
-const mongoose = require('mongoose')
-const db = mongoose.connection
+const db = require('../../config/mongoose')
+
 const Todo = require('../todo')
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-
-db.on('error', () => {
-  console.log('mongdb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: 'name-' + i })
   }
