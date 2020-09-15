1d0c9d9e1ea34d19437fdd645db5de5c4d8171de
Bill <reborn800201@gmail.com>
2020-09-15 18:17:37 +0800

    feat modify index page ui

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
-require('./config/mongooes')
+require('./config/mongoose')
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 // 設定首頁路由
-app.ues(routes)
+app.use(routes)
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('.modules/home')
-const todos = require('.modules/todos')
+const home = require('./modules/home')
+const todos = require('./modules/todos')
 
 router.use('/', home)
 router.use('/todos', todos)
