e800a69ddac091e2152106aff9a546e392d13d7c
Bill <reborn800201@gmail.com>
2020-11-02 17:08:41 +0800

    feat: add user modules

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,26 +4,8 @@ const Todo = require('../../models/todo')
 let temptime = 0
 
 
-router.get('/', (req, res) => {
-  let dt = new Date
-  let DateTime = `${dt.getFullYear()}/${dt.getMonth() + 1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
 
-  if (temptime === 0) {
-    console.log(DateTime + ' | ' + req.method + ' from ' + req.originalUrl)
-  } else {
-    console.log(DateTime + ' | ' + req.method + ' from ' + req.originalUrl + ' | ues time:' + (dt - temptime) + 'ms')
-  }
-
-  temptime = 0
-  Todo.find()
-    .lean().sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos: todos }))
-    .catch(error => console.error(error))
-})
-
-
-
-router.get('/todos/new', (req, res) => {
+router.get('/new', (req, res) => {
   let dt = new Date
   let DateTime = `${dt.getFullYear()}/${dt.getMonth() + 1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
   console.log(DateTime + ' | ' + req.method + ' from ' + req.originalUrl)
@@ -33,7 +15,7 @@ router.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
-router.post('/todos', (req, res) => {
+router.post('/', (req, res) => {
   const name = req.body.name
 
   const todo = new Todo({ name })
@@ -48,7 +30,7 @@ router.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.get('/todos/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   let dt = new Date
   let DateTime = `${dt.getFullYear()}/${dt.getMonth() + 1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
@@ -62,7 +44,7 @@ router.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.get('/todos/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   let dt = new Date
   let DateTime = `${dt.getFullYear()}/${dt.getMonth() + 1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
@@ -73,7 +55,7 @@ router.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.put('/todos/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const id = req.params.id
   const name = req.body.name
   const isDone = req.body.isDone
@@ -96,7 +78,7 @@ router.put('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.delete('/todos/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id
   let dt = new Date
   let DateTime = `${dt.getFullYear()}/${dt.getMonth() + 1}/${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
